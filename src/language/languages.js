e64a2fbfa7334cea3aeb5d62859bedc3f53c053a
Elisei <strashevsky.elisey@gmail.com>
2023-11-06 19:25:18 +0300

    changes to resume

--- src/language/languages.js
+++ src/language/languages.js
@@ -15,7 +15,7 @@ export const text = {
   "Contact": { en: "Contact", ru: "Связь" },
   "AboutMe_1": {
     en: "With over a year of commercial experience, I write in JavaScript, TypeScript, React and Vue. I take great pleasure in designing responsive and cross-browser interfaces that enhance user experiences. ",
-    ru: "В целом имею больше года опыта коммерческой разработки. Пишу на JavaScript/TypeScript, пользуюсь React и Vue в качестве основных фреймворков. По части других технологий - Вы можете посмотреть ",
+    ru: "В целом имею больше года опыта коммерческой разработки. Пишу на JavaScript/TypeScript, для работы и в проектах использую Vue и React. По части других технологий - Вы можете посмотреть ",
   },
   "resume": { en: "My resume", ru: "мое резюме," },
   "AboutMe_2": {
