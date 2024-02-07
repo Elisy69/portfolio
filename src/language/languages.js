dd4311626d6630d88f98902215c09c8634b84ea8
Elisei <strashevsky.elisey@gmail.com>
2024-02-07 15:44:37 +0300

    fix

--- src/language/languages.js
+++ src/language/languages.js
@@ -1,45 +1,45 @@
 export const text = {
   "Hello!": { en: "Hello!", ru: "Привет!" },
   "I'm": { en: "I'm", ru: "Я" },
-  "Elisei": { en: "Elisei", ru: "Елисей" },
-  "developing": { en: "developing", ru: "разрабатываю" },
-  "Creative": { en: "Creative", ru: "Креативные" },
-  "Interactive": { en: "Interactive", ru: "Интерактивные" },
-  "webapps": { en: "webapps", ru: "веб-приложения" },
-  "workstatus": { en: "Available for work", ru: "Вакансия открыта" },
-  "English": { en: "English", ru: "Английский" },
-  "Russian": { en: "Russian", ru: "Русский" },
-  "Home": { en: "Home", ru: "Наверх" },
-  "About": { en: "About", ru: "Опыт" },
-  "Projects": { en: "Projects", ru: "Проекты" },
-  "Contact": { en: "Contact", ru: "Связь" },
-  "AboutMe_1": {
+  Elisei: { en: "Elisei", ru: "Елисей" },
+  developing: { en: "developing", ru: "разрабатываю" },
+  Creative: { en: "Creative", ru: "Креативные" },
+  Interactive: { en: "Interactive", ru: "Интерактивные" },
+  webapps: { en: "webapps", ru: "веб-приложения" },
+  workstatus: { en: "Available for work", ru: "Вакансия открыта" },
+  English: { en: "English", ru: "Английский" },
+  Russian: { en: "Russian", ru: "Русский" },
+  Home: { en: "Home", ru: "Наверх" },
+  About: { en: "About", ru: "Опыт" },
+  Projects: { en: "Projects", ru: "Проекты" },
+  Contact: { en: "Contact", ru: "Связь" },
+  AboutMe_1: {
     en: "With over two years of coding experience, I write in JavaScript, TypeScript, React and Vue. I take great pleasure in designing responsive and cross-browser interfaces that enhance user experiences. ",
-    ru: "В целом имею два года опыта разработки. Пишу на JavaScript/TypeScript, для работы и в проектах использую Vue и React.",
+    ru: "В целом имею более года опыта разработки. Пишу на JavaScript/TypeScript, для работы и в проектах использую Vue и React.",
   },
-  "resume": { en: "My resume", ru: "мое резюме," },
-  "AboutMe_2": {
+  resume: { en: "My resume", ru: "мое резюме," },
+  AboutMe_2: {
     en: "I am eager to learn new technologies that your team is working with. Additionally, I have a C1-C2 proficiency in English.",
     ru: " Я открыт к максимально быстрому освоению любых новых для меня технологий, которые использует ваша команда. Также я свободно владею английским, что очень сильно помогает самостоятельно находить решение проблем/сразу лезть в документацию за разъяснениями.",
   },
-  "projectsTitle": { en: "Featured Projects", ru: "Примеры проектов" },
-  "contacts": {
+  projectsTitle: { en: "Featured Projects", ru: "Примеры проектов" },
+  contacts: {
     en: "Interested? Hit me up!",
     ru: "Интересно? Пишите!",
   },
-  "budgetApp": {
+  budgetApp: {
     en: "After tediously searching for a budget app on the App Store that could perform its core functions without unnecessary complications and features, I had the idea to create my own. This app is designed to manage both income and expenses, offering straightforward budget analysis and the ability to set long-term financial goals. Users can also plan their budgets in various currencies, as the app automatically retrieves the latest exchange rates through an open exchange rates API.",
     ru: "После долгих поисков приложения для планирования бюджета в App Store, которое просто могло бы выполнять свои функции без нагромождения бесполезных фич, у меня появилась идея написать свое. Данное приложение считает бюджет по месяцам, предлагает анализ расходов/доходов пользователя, а также позволяет ставить финансовые цели. Пользователи также могут планировать свои бюджеты в различных валютах, так как приложение автоматически получает актуальные курсы обмена через открытое API.",
   },
-  "calendarApp": {
+  calendarApp: {
     en: "This app can tremendously help people who work in shifts because it's challenging to plan your life without knowing when your days off are for the next month. The app adapts to any work schedule using a simple algorithm to calculate workdays and days off. The calendar is built from scratch without using any libraries.",
     ru: "Это приложение может очень помочь людям, работающим по сменам, так как сложно планировать свою жизнь, не зная, когда будут выходные в следующем месяце. Приложение адаптируется под любой график работы с использованием простого алгоритма расчета рабочих дней и выходных. Календарь создан с нуля, без использования каких-либо библиотек.",
   },
-  "bookSearchApp": {
+  bookSearchApp: {
     en: "This app empowers you to discover any book you desire, utilizing an open API provided by Google. You can simply click on a book to view all its details.",
     ru: "Это приложение поможет вам отыскать любую книгу, используя открытое API, предоставленное Google. Просто кликните на книгу, чтобы увидеть все ее подробности.",
   },
-  "pizzaApp": {
+  pizzaApp: {
     en: "Work in progress: a pizza ordering app written in Vue.js. One of the core features is to provide users with the ability to create their custom pizzas using a special interface.",
     ru: "В работе. Приложение для заказа пиццы, написанное с помощью Vue.js. Главная фича - предоставление пользователю возможности создавать свою собственную пиццу, используя специальный интерфейс.",
   },
